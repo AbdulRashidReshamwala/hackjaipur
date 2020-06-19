f4f4b4854aebd982ee17d7f6db6dc1aa956ade16
Abdul Rashid <ar1242112@gmail.com>
2020-06-20 00:51:14 +0530

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfGZjND6kgA3Sx7G9OYhnLRFdwjeeWXPc",
+  authDomain: "hackjaipur-95690.firebaseapp.com",
+  databaseURL: "https://hackjaipur-95690.firebaseio.com",
+  projectId: "hackjaipur-95690",
+  storageBucket: "hackjaipur-95690.appspot.com",
+  messagingSenderId: "973190574578",
+  appId: "1:973190574578:web:ce4c8200f61d075381fb6b",
+  measurementId: "G-MY0HQ9D1JD",
+};
