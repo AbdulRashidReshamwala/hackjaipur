8b27547eff40b0933d5c7bb1e25c23e112fc5b21
ubaidkolad <bhaid023@gmail.com>
2020-06-20 17:28:20 +0530

    Landing page without styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import AboutPage from "./Pages/About/AboutPage";
 import CallPage from "./Pages/Call/CallPage";
 import CreatePage from "./Pages/Create/CreatePage";
+import LandingPage from './Pages/LandingPage/LandingPage'
+import Navbar from "./Core/Navbar";
+import Login from "./AuthPages/Login";
+
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/create">
-          <CreatePage />
-        </Route>
-        <Route path="/call">
-          <CallPage />
-        </Route>
-        <Route path="/">
-          <AboutPage />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path="/create">
+            <CreatePage />
+          </Route>
+          <Route path="/call">
+            <CallPage />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/login" component={ Login }/>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
