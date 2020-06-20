349aea5b7518894324c7ce2ad9d5108810789671
Sajjad21990 <sajjadhaider21990@gmail.com>
2020-06-20 21:00:20 +0530

    hyy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAfGZjND6kgA3Sx7G9OYhnLRFdwjeeWXPc",
+  authDomain: "hackjaipur-95690.firebaseapp.com",
+  databaseURL: "https://hackjaipur-95690.firebaseio.com",
+  projectId: "hackjaipur-95690",
+  storageBucket: "hackjaipur-95690.appspot.com",
+  messagingSenderId: "973190574578",
+  appId: "1:973190574578:web:17f219227b1e074381fb6b",
+  measurementId: "G-2JMTF41WH3",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
