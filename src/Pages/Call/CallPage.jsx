a3a3139e363b0e7b4fcbab363e4019a5463b8331
Abdul Rashid <ar1242112@gmail.com>
2020-06-20 17:31:00 +0530

    added user stream element

--- src/Pages/Call/CallPage.jsx
+++ src/Pages/Call/CallPage.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import adapter from "webrtc-adapter";
 
+const mediaStreamConstraints = {
+  video: true,
+};
 export default function CallPage() {
-  return <div>Call</div>;
+  const getUserMedia = async () => {
+    try {
+      let ms = await navigator.mediaDevices.getUserMedia(
+        mediaStreamConstraints
+      );
+      document.querySelector("#my-stream").srcObject = ms;
+    } catch {
+      alert("No media Devices Found");
+    }
+  };
+  return (
+    <div>
+      <button onClick={getUserMedia}>hello</button>
+      <video id="my-stream" autoPlay playsInline></video>
+      <video id="remoteVideo" autoPlay playsInline></video>
+      <div>
+        <button id="startButton">Start</button>
+        <button id="callButton">Call</button>
+        <button id="hangupButton">Hang Up</button>
+      </div>
+    </div>
+  );
 }
